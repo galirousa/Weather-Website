812de90fc4690dbdce8019fb78accf5114d55fcf
Xohan Otero Barbosa <xohan.otero@gmail.com>
2018-06-11 17:45:57 +0200

    Initial Push

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Weather Website
+This is a website that just shows the current weather for the city you look for.
+It doesn’t do anything special for now as it is a work in progress.
+It’s powered by the P5js library and Open Weather Map.
---
+++ weather.js
@@ -0,0 +1,43 @@
+var sunImg;
+var cloudImg;
+var weather;
+var api = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var apiKey = '&&appid=f2cc39d6d38c6da57d95d0e6a5a800da';
+var units='&units=metric';
+var input;
+function setup(){
+  sunImg = loadImage("data/sun.png");
+  cloudImg = loadImage("data/clouds.png");
+  createCanvas(300,300);
+  var button = select('#button');
+  button.mousePressed(weatherAsk);
+  input = select('#city');
+}
+function weatherAsk(){
+  var url = api+input.value()+apiKey+units;
+  loadJSON(url,gotData);
+}
+function gotData(data){
+  weather = data;
+}
+function draw(){
+  clear();
+if(weather){
+  if(weather.clouds.all>50){
+    background(cloudImg);
+  } else if(weather.clouds.all<50){
+    background(sunImg);
+  }
+  noStroke();
+  fill(0);
+  text("Temperature: " +weather.main.temp+"C",10,230);
+  text("Cloudyness %: "+weather.clouds.all,10,245);
+  text("Humidity %: " + weather.main.humidity+"%",10,260)
+  text("Minimun temp: " +weather.main.temp_min+"C",10,275);
+  text("Maximum temp: " +weather.main.temp_max+"C",10,290);
+
+
+
+
+  }
+}
